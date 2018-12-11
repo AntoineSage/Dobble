cdf172f5d225aab0e6372431b52e0b4caceb677a
rollinal <alexis.rollin29@orange.fr>
2018-12-11 10:23:39 +0100

    Fonctions de detection d'icone correspondant au clic

--- src/icone.c
+++ src/icone.c
@@ -1,1 +1,12 @@
 #include <icone.h>
+#include "math.h"
+
+bool estDansIcone(Icone I, int x, int y){
+	bool dedans=false;
+	float xcentre=I.distance*cos((float)(I.angle));
+	float ycentre=I.distance*sin((float)(I.angle));
+	if (sqrt((xcentre-x)^2+(ycentre-y)^2)<I.taille*0.5){
+		dedans=true;
+	}
+	return dedans;
+}
--- src/plateau.c
+++ src/plateau.c
@@ -1,1 +1,14 @@
-#include <plateau.h>
+#include "plateau.h"
+
+Image Clic_a_l_interieur(Plateau P, int xclic, int yclic){
+	
+	int i;
+	Image I=-1;
+	Carte H=P.haut;
+	for (i=0; i<H.nbIcones;i++){
+		if (estDansIcone(H.icones[i],xclic,yclic){
+			I=H.icones[i].image;
+		}
+	}
+	return I;
+}
